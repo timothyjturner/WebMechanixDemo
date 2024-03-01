164e2575e4e7b462995752377a045ab16d59b55b
timothyjturner <timothyjturner@gmail.com>
2024-03-01 17:37:16 -0500

    block fix

--- blocks.php
+++ blocks.php
@@ -2,5 +2,17 @@
 
 add_action( 'init', 'register_acf_blocks' );
 function register_acf_blocks() {
-    register_block_type( __DIR__ . './blocks/hero/block.json' );
+   // register_block_type( __DIR__ . './blocks/hero/block.json' );
+   
+    acf_register_block_type(array(
+    'name'              => 'hero',
+    'title'             => __('Hero'),
+    'description'       => __('A custom hero block.'),
+		'render_template'   => './blocks/hero/hero.php',
+		'render_callback'	=> 'my_acf_block_render_callback',
+    'category'          => 'formatting',
+    'icon'              => 'superhero',
+		'keywords'          => array( 'hero' ),
+		'mode' 	=> 'edit'
+ ));
 }
